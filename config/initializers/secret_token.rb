42838db16dceddc5cfe1fd62cb0924974ae47401
Chris Westra <cwestra@gmail.com>
2013-09-28 16:21:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThreeCBreweryCagematch::Application.config.secret_key_base = '985a5bae6cb006c28408db64cfdac1c3b2bb72b3c1d5c5d207d2356ab942f03b4e22733c5012ff68637456fe8c1df522194ee4e161b3f40ccf2eb63ff414b950'
