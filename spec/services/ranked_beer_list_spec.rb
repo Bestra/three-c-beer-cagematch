5247222e1d5472975f5730aaf0ac488daf7504a4
Chris Westra <cwestra@gmail.com>
2013-09-29 09:22:56 -0400

    Expect the best ranked beer to come first.

--- spec/services/ranked_beer_list_spec.rb
+++ spec/services/ranked_beer_list_spec.rb
@@ -8,9 +8,9 @@ describe RankedBeerList do
   end
 
   it "gives a same-ranked beer with more votes a better score than a beer with fewer votes" do
-    beers = [stub_beer(3.0, 7), stub_beer(3.01, 1)]
+    beers = [stub_beer(3.0, 7), stub_beer(3.0, 1), stub_beer(2.0, 1)]
     ranked_beers = RankedBeerList.new(beers).ranked_beers
-    ranked_beers[0][:rank].should be < ranked_beers[1][:rank]
+    ranked_beers[0][:rank].should be > ranked_beers[1][:rank]
     ranked_beers[0][:beer].votes.should == 7
   end
 
