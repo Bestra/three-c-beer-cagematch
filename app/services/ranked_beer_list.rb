c42dd6afc6dedb2827f1d418c47e649b66bed88c
Chris Westra <cwestra@gmail.com>
2013-09-29 09:23:13 -0400

    Use BeerAdvocate's ranking algorithm.

--- app/services/ranked_beer_list.rb
+++ app/services/ranked_beer_list.rb
@@ -5,29 +5,34 @@ class RankedBeerList
     @considered_beers = beers
   end
 
-  # use a bayesian ranking for the beers
-  # (average_votes * average_rating)
+  MIN_VOTES = 5
+
   def ranked_beers
     self.considered_beers.map { |b| {rank: rank_beer(b), beer: b} }
-    .sort_by { |i| i[:rank] }
+    .sort_by { |i| i[:rank] }.reverse!
   end
 
   private
 
+  # Use BeerAdvocates ranking method to sort the beers.
+  # weighted rank (WR) = (v ÷ (v+m)) × R + (m ÷ (v+m)) × C
+  # where:
+  # R = review average for the beer
+  # v = number of reviews for the beer
+  # m = minimum reviews required to be listed (currently 10)
+  # C = the mean across the list (currently 2.5)
   def rank_beer(beer)
-    kA = (average_votes * average_rating)
-    kB = (beer.votes * beer.rAvg)
-    kC = (average_votes + beer.votes)
-    (kA + kB) / kC
+    v = beer.votes.to_f
+    m = MIN_VOTES.to_f
+    kA = (v / (v + m)) * beer.rAvg
+    kB = (m / (v + m)) * average_rating
+    kA + kB
   end
 
   def average(property)
     considered_beers.map(&property).inject(:+) / considered_beers.count
   end
 
-  def average_votes
-     @average_votes ||= average :votes
-  end
 
   def average_rating
      @average_rating ||= average :rAvg
