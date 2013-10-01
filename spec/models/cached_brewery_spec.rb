6b0a38ad9f847adf8f45d7fe266db045b87816c1
Chris Westra <cwestra@gmail.com>
2013-09-30 22:37:05 -0400

    Add spec for saving cached breweries.

--- spec/models/cached_brewery_spec.rb
+++ spec/models/cached_brewery_spec.rb
@@ -4,12 +4,21 @@ describe CachedBrewery do
   let(:beer1) { Beer.new "Bud", "Pilsner", "?", "3", "12", "/1", "/1" }
   let(:beer2) { Beer.new "Lite", "Pilsner", "?", "4", "12", "/2", "/1" }
   let(:url) { "/beer/1" }
+  let(:test_name) { "Test Brewery" }
+  let(:test_brewery) do
+    b = Brewery.new
+    b.name = test_name
+    b.city_name = "Columbus"
+    b.profile_url = url
+    b.beers = [beer1, beer2]
+    b
+  end
 
   it "returns a cached brewery from a url" do
-    CachedBrewery.create!(profile_url: url, name: "Test", city_name: "Columbus", beers: [beer1, beer2])
+    CachedBrewery.create!(profile_url: url, name: test_name, city_name: "Columbus", beers: [beer1, beer2])
     b = CachedBrewery.for_url url
     b.should be_a Brewery
-    b.name.should == "Test"
+    b.name.should == test_name
     b.beers[0].name.should == "Bud"
     b.beers[1].name.should == "Lite"
   end
@@ -28,6 +37,13 @@ describe CachedBrewery do
   end
 
 
-  it "saves a brewery to the cache"
+  it "saves a brewery to the cache" do
+    CachedBrewery.save test_brewery
+    b = CachedBrewery.for_url url
+    b.should be_a Brewery
+    b.name.should == test_name
+    b.beers[0].name.should == "Bud"
+    b.beers[1].name.should == "Lite"
+  end
 
 end
