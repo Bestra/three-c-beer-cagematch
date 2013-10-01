2fbfe581b6bec50889faccdc04183da02c6492c6
Chris Westra <cwestra@gmail.com>
2013-10-01 08:19:33 -0400

    Create BeerRanking and initial spec.

---
+++ app/services/beer_ranking.rb
@@ -0,0 +1,23 @@
+class BeerRanking
+  PROFILE_URLS = {
+    columbus: "http://beeradvocate.com/beerfly/city/40",
+    cincinatti: "http://beeradvocate.com/beerfly/city/39",
+    cleveland: "http://beeradvocate.com/beerfly/city/5"
+  }
+
+  def self.beer_ranking(options={})
+    city_names = options[:cities] || %i(columbus cleveland cincinatti)
+    style = options[:style]
+
+    cities = city_names.map do |name|
+      url = PROFILE_URLS[name]
+      if url
+        City.create_from_url url, name.capitalize
+      end
+    end.compact
+
+    all_beers = cities.flat_map(&:beers)
+
+  end
+
+end
---
+++ spec/services/beer_ranking_spec.rb
@@ -0,0 +1,9 @@
+require_relative "../../app/services/beer_ranking"
+describe BeerRanking do
+
+  it "returns a weighted list of beers from the 3 Cs."
+
+  it "can specify cities to search"
+
+  it "can filter beers by style name"
+end
