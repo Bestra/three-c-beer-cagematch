82df4a2382f9e0f59d0b1cd57a0a28a22f6eae05
Chris Westra <cwestra@gmail.com>
2013-10-01 08:51:02 -0400

    Start implementing city beer listing.

--- app/services/beer_ranking.rb
+++ app/services/beer_ranking.rb
@@ -5,14 +5,14 @@ class BeerRanking
     cleveland: "http://beeradvocate.com/beerfly/city/5"
   }
 
-  def self.beer_ranking(options={})
+  def self.beer_list(options={})
     city_names = options[:cities] || %i(columbus cleveland cincinatti)
     style = options[:style]
 
     cities = city_names.map do |name|
       url = PROFILE_URLS[name]
       if url
-        City.create_from_url url, name.capitalize
+        City.create_from_url url, name.to_s.capitalize
       end
     end.compact
 
--- spec/services/beer_ranking_spec.rb
+++ spec/services/beer_ranking_spec.rb
@@ -1,9 +1,30 @@
 require_relative "../../app/services/beer_ranking"
+require_relative "../../app/services/city"
+require_relative "../../app/services/beer"
 describe BeerRanking do
+  #make a beer with randomized ranking and votes
+  def stub_beer(brewery_name, city_name)
+     b = Beer.new "Bud", "Pilsner", "?", rand + rand(4), rand(100), "/1", "/1"
+     b.stub(brewery_name: brewery_name)
+     b.stub(city_name: city_name)
+     b
+  end
 
-  it "returns a weighted list of beers from the 3 Cs."
+  it "returns a beer list" do
+     City.stub(:create_from_url) do |url, city_name|
+       c = City.new
+       c.stub(:beers).and_return [stub_beer("A Brewery", city_name)]
+       c
+     end
+     BeerRanking.beer_list.count.should == 3
+  end
 
-  it "can specify cities to search"
+  describe "specifying cities to search" do
+    it "only returns results from the specified cities"
+
+    it "returns nothing from an invalid city name"
+
+  end
 
   it "can filter beers by style name"
 end
